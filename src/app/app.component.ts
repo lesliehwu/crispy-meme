915cb38ebd9d8f639209f26c12ac2a7cf088d6e5
lesliehwu <mslesliewu@gmail.com>
2017-10-24 17:09:16 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'SWITCHY';
+    switches=[true,false,true,false,true,false];
+    flipSwitch(x){
+        this.switches[x]=!this.switches[x];
+    }
 }
